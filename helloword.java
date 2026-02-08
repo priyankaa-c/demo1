fccd3cf502d245a93f063f98b23c65d0f0501fc2
priyankaa-c <155766880+priyankaa-c@users.noreply.github.com>
2026-02-08 13:33:22 +0530

    Fix typo in welcome message

--- helloword.java
+++ helloword.java
@@ -1,5 +1,5 @@
  public class helloword {
 	public static void main(String[] args) {
-		System.out.println("welcome to Jenkins world new again try try ");
+		System.out.println("welcome to Jenkins world new again try try try");
 	}
 }
