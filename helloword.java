36def0e1d93615134c901565389a3610b8982f80
priyankaa-c <155766880+priyankaa-c@users.noreply.github.com>
2026-02-08 13:40:52 +0530

    Update helloword.java

--- helloword.java
+++ helloword.java
@@ -1,5 +1,5 @@
  public class helloword {
 	public static void main(String[] args) {
-		System.out.println("welcome to Jenkins world new again try try try script try");
+		System.out.println("welcome to Jenkins world new again try try try script try sh");
 	}
 }
